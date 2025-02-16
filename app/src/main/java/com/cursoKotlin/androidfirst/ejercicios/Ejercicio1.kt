7d0bcdc28a5fa23d20cf86123f202bf459661b8f
Julian Ramos <julianc.ramos21@gmail.com>
2025-02-16 18:30:33 -0500

    Se agrega el paquete de ejercicios y se realiza el numero 1

---
+++ app/src/main/java/com/cursoKotlin/androidfirst/ejercicios/Ejercicio1.kt
@@ -0,0 +1,20 @@
+package com.cursoKotlin.androidfirst.ejercicios
+
+import android.app.Notification
+
+//Link al ejercicio en:
+//https://developer.android.com/codelabs/basic-android-kotlin-compose-kotlin-fundamentals-practice-problems?hl=es-419#1
+fun main(){
+    val morningNotification = 51
+    val eveningNotification = 135
+
+    printNotificationSummary(morningNotification)
+    printNotificationSummary(eveningNotification)
+}
+
+fun printNotificationSummary (notification: Int){
+    when (notification){
+        in 0..99 -> println("You have $notification notifications")
+        else -> println("Your phone is blowing up! You have 99+ notifications.")
+    }
+}
