c16411b80349f6e8f88e05c8189763de62a03c29
Julian Ramos <julianc.ramos21@gmail.com>
2025-02-16 14:40:40 -0500

    Variables en Kotlin

--- app/src/main/java/com/cursoKotlin/androidfirst/Variables.kt
+++ app/src/main/java/com/cursoKotlin/androidfirst/Variables.kt
@@ -1,5 +1,49 @@
 package com.cursoKotlin.androidfirst
 
 fun main(){
-    print("HOLA MUNDO")
+    //No hay necesidad de hacer el tipo de variable explícito
+    val numero1 =42
+    val numero: Int = 42
+    println("Numero no explicito:  $numero1" )
+    println("Numero explicito:  $numero" )
+
+    val texto: String = "Hola mundo"
+    val decimal: Double = 3.14
+    val caracter: Char = 'K'
+    val esValido: Boolean = true
+    println(texto)
+    println(decimal)
+    println(caracter)
+    println(esValido)
+
+    //Insertar variables en cadenas:
+    val nombre = "Julian"
+    val saludo = "Hola, $nombre"
+    println(saludo)
+    //Tambien se puede con expresiones complejas:
+    val edad = 25
+    val mensaje = "En 5 años tendrás ${edad + 5} años"
+    println(mensaje)
+
+    //Hay que realizar conversión explícita entre tipos de variables:
+    val num: Int = 10
+    val numLong: Long = num.toLong()  // Conversión explícita
+    println(numLong)
+    /*Tambien hay:
+    toInt(): Convierte a Int.
+    toLong(): Convierte a Long.
+    toDouble(): Convierte a Double.
+    toFloat(): Convierte a Float.
+    toShort(): Convierte a Short.
+    toByte(): Convierte a Byte.
+     */
+
+    //Para texto con multiples lineas se puede poner comilla triple (""")
+    val textoLargo = """
+        Kotlin es genial.
+        Se pueden poner varias lineas.
+    """
+    println(textoLargo)
+    println("Fin de las variables")
+
 }
