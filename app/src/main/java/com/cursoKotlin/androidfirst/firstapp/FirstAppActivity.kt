e500a4fac7501fde3ad06ed70f38e84413c60752
Julian Ramos <julianc.ramos21@gmail.com>
2025-02-19 22:57:22 -0500

    Se añade View Binding por primera vez

--- app/src/main/java/com/cursoKotlin/androidfirst/firstapp/FirstAppActivity.kt
+++ app/src/main/java/com/cursoKotlin/androidfirst/firstapp/FirstAppActivity.kt
@@ -6,16 +6,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.cursoKotlin.androidfirst.R
+import com.cursoKotlin.androidfirst.databinding.ActivityFirstAppBinding
 
 class FirstAppActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFirstAppBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_first_app)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Inflar el layout usando View Binding
+        binding = ActivityFirstAppBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+//        Para adaptar padding
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 }
